40639bcde28c8b38b146539e99efdeb22561db94
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2025-06-29 18:20:53 +0400

    added push notification

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,9 +1,28 @@
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { theme } from "../../theme";
+import { registerForPushNotificationsAsync } from "../../utils/registerForPushNotificationsAsync";
 
 export default function CounterScreen() {
+  const scheduleNotification = async () => {
+    const result = await registerForPushNotificationsAsync();
+    if (result === "granted") {
+      console.log("Permission: ", result);
+    } else {
+      Alert.alert(
+        "Unable to schedule notification",
+        "Enable the notifications permission for Expo Go in settings"
+      );
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Counter</Text>
+      <TouchableOpacity
+        onPress={scheduleNotification}
+        activeOpacity={0.8}
+        style={styles.button}
+      >
+        <Text style={styles.buttonText}>Schedule notification</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,7 +34,15 @@ const styles = StyleSheet.create({
     alignItems: "center",
     backgroundColor: "#fff",
   },
-  text: {
-    fontSize: 24,
+  button: {
+    backgroundColor: theme.colorBlack,
+    padding: 12,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: "#fff",
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    letterSpacing: 1,
   },
 });
