9212a56628a8592d4172f17471a94d36429c8f57
Sayyid Marvan <sayyidmarvanvt@gmail.com>
2025-06-30 16:43:45 +0400

    fixed history button

--- app/counter/_layout.tsx
+++ app/counter/_layout.tsx
@@ -1,5 +1,6 @@
 import { Link, Stack } from "expo-router";
 import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
 import { theme } from "../../theme";
 import { Pressable } from "react-native";
 
@@ -10,19 +11,19 @@ export default function Layout() {
         name="index"
         options={{
           title: "Counter",
-          headerRight: () => (
-            <Link href={"counter/history"} asChild>
-              <Pressable hitSlop={20} 
-              style={{ backgroundColor: "white" }}>
-                {/*hitslop give large area for touch , give background white because some time icon get hidden*/}
-                <MaterialIcons
-                  name="history"
-                  size={28}
-                  color={theme.colorGrey}
-                />
-              </Pressable>
-            </Link>
-          ),
+          headerRight: () => {
+            return (
+              <Link href="/counter/history" asChild>
+                <Pressable hitSlop={20} style={{ backgroundColor: "white", height:32}}>
+                  <MaterialIcons
+                    name="history"
+                    size={32}
+                    color={theme.colorGrey}
+                  />
+                </Pressable>
+              </Link>
+            );
+          },
         }}
       />
       <Stack.Screen name="history" options={{ title: "History" }} />
